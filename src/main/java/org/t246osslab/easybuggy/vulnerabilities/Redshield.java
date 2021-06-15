af709d749feba57d25f33e3101347468def841da
TimWhiteSource <tim.franklin@whitesourcesoftware.com>
2021-06-15 15:50:47 -0500

    redshield

--- src/main/java/org/t246osslab/easybuggy/vulnerabilities/Redshield.java
+++ src/main/java/org/t246osslab/easybuggy/vulnerabilities/Redshield.java
@@ -10,6 +10,7 @@ import java.util.Locale;
 
 public class Redshield extends UnrestrictedSizeUploadServlet {
 // test
+    //
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
